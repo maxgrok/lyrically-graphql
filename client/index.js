24e112e8298153de0bd40952fcf2a666ee341c3c
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 13:45:45 -0500

    importing libraries and creating apollo provider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'; //interacting with teh graphql server and making requests for data nd storing that data locally when response comes back
+
+import { ApolloProvider } from 'react-apollo'; // provider of data, injects data into react
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return <ApolloProvider>
+    <div>Lyrical</div>
+  </ApolloProvider>
 };
 
 ReactDOM.render(
