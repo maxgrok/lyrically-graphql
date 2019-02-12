9e2c5483cb91cd4b9b960d6b80d07cd3ad13f3f1
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 14:44:47 -0500

    adding renderSongs helper method

--- client/components/songList.js
+++ client/components/songList.js
@@ -4,11 +4,19 @@ import React, {Component} from 'react';
 import gql from 'graphql-tag'; //helper that allows us to write queries within a component file
 import { graphql } from 'react-apollo'; // react-apollo provides bonding layer between graphql server with react side of application
 class SongList extends Component{
+    renderSongs(){
+        return this.props.data.songs.map(song => {
+            return (
+                <li>
+                    {song.title}
+                </li>
+            )
+        });
+    }
+
     render(){
-        console.log(this.props);
-        
         return (<div>
-            SongList
+            {this.renderSongs()}
         </div>
         )
     }
