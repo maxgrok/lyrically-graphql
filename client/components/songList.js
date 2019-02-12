c09c0e393671e0351b85367d9352aeed7f13b68e
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 14:38:49 -0500

    adding graphql bonding layer and syntax for export default

--- client/components/songList.js
+++ client/components/songList.js
@@ -2,9 +2,11 @@
 
 import React, {Component} from 'react';
 import gql from 'graphql-tag'; //helper that allows us to write queries within a component file
-
+import { graphql } from 'react-apollo'; // react-apollo provides bonding layer between graphql server with react side of application
 class SongList extends Component{
     render(){
+        console.log(this.props);
+        
         return (<div>
             SongList
         </div>
@@ -19,4 +21,4 @@ const query = gql`
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
