dd101cea119286aba0d3a11626c1aa9c0cb885b3
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 15:00:32 -0500

    adding react router import

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import ApolloClient from 'apollo-client'; //interacting with teh graphql server and making requests for data nd storing that data locally when response comes back
 import SongList from './components/SongList';
 
 import { ApolloProvider } from 'react-apollo'; // provider of data, injects data into react
+
 
 const client = new ApolloClient({}); //makes assumptions about how your backend is setup
 
