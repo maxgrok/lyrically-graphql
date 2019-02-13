f219f68dafe9eb8fa288fea14c747083fc653340
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 19:42:24 -0500

    creating repeatable queries

--- client/components/songList.js
+++ client/components/songList.js
@@ -4,13 +4,14 @@ import React, {Component} from 'react';
 import gql from 'graphql-tag'; //helper that allows us to write queries within a component file
 import { graphql } from 'react-apollo'; // react-apollo provides bonding layer between graphql server with react side of application
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component{
     renderSongs(){
         return this.props.data.songs.map(song => {
             return (
                 <li key={song.id} className="collection-item">
-                    {song.title}
+                    {song.title} 
                 </li>
             )
         });
@@ -35,13 +36,5 @@ class SongList extends Component{
         )
     }
 }
-const query = gql`
-{
-    songs{
-      id
-      title
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
