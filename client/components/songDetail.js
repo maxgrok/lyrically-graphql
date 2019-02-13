e4a92abc3628bd117a45cb8fc44e15e03f0e1c23
Max Goodman <max.goodman@flatironschool.com>
2019-02-13 10:16:54 -0500

    adding mutation for the lyricCreate.js component

--- client/components/songDetail.js
+++ client/components/songDetail.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { graphql } from 'react-apollo';
 import {Link} from 'react-router';
 import fetchSong from '../queries/fetchSong';
-
+import LyricCreate from './LyricCreate';
 class SongDetail extends Component{
     render(){
         const { song } = this.props.data;
@@ -14,6 +14,7 @@ class SongDetail extends Component{
             <div>
                 <Link to="/">Back</Link>
                 <h3>{song.title}</h3>
+                <LyricCreate songId={this.props.params.id} />
             </div>
         )
     }
